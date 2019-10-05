ac826ef7bbf8f24f0e9e389699844d3ab4eb03fc
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-10-05 21:41:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ main.remove();
 
 let newHeader = document.createElement('h1');
 newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = 'Name is the champrion'
 newHeader.apendChild(body);
 
