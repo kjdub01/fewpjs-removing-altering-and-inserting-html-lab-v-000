e5a1eaff2bc0809a5d7f5e412dc51a580d785e18
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2019-10-05 21:37:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ main.remove();
 
 let newHeader = document.createElement('h1');
 newHeader.setAttribute('id', 'victory');
+newHeader.innerHtml = `${name} is the champion`
 newHeader.apendChild(body);
